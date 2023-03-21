41a4cd7d78f2446509c18a47d4ac70842b8df3e7
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-21 18:44:01 +0100

    Add files via upload

---
+++ 01-lesson/src/App.js
@@ -0,0 +1,12 @@
+import Counter from './features/Counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <main className='App'>
+      <Counter/>
+    </main>
+  );
+}
+
+export default App;
