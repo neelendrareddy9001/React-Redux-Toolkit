fe8fed4053b0d7bb05b36e079e4f5f1a683ead28
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-21 21:54:41 +0100

    Update PostList.js

--- 02_lesson/src/features/PostList.js
+++ 02_lesson/src/features/PostList.js
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postSlice";
+import PostAuthor from "./PostAuthor";
 
 const PostList = () => {
     const posts = useSelector(selectAllPosts)
@@ -8,6 +9,9 @@ const PostList = () => {
         <article key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.content.substring(0, 100)}</p>
+            <p className="postCredit">
+                <PostAuthor userId={post.userId}/>
+            </p>
         </article>
     ))
 
