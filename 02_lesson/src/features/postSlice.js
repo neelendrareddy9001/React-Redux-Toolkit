1e6364f48751695f527aff27dc3ca7c83631daeb
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-03-21 18:37:52 +0100

    React-Redux-Toolkit

---
+++ 02_lesson/src/features/postSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+    {id : '1', title : 'Learning Redux Toolkit', content: "I've heard good thing "},
+    {id : '2', title : 'Slices...', content : "The more I say slice, the more I want pizza."}
+]
+
+
+const postSlice = createSlice({
+    name : 'posts',
+    initialState,
+    reducers : {
+
+    }
+});
+
+export const selectAllPosts = (state) => state.posts;
+
+export default postSlice.reducer
