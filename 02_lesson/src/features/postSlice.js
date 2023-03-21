70068018eea05f5e8e538ad5e6c89828fb6863e2
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-21 20:11:58 +0100

    Update postSlice.js

--- 02_lesson/src/features/postSlice.js
+++ 02_lesson/src/features/postSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice , nanoid} from "@reduxjs/toolkit";
 
 const initialState = [
     {id : '1', title : 'Learning Redux Toolkit', content: "I've heard good thing "},
@@ -10,10 +10,24 @@ const postSlice = createSlice({
     name : 'posts',
     initialState,
     reducers : {
-
+        postAdded : {
+            reducer (state, action) {
+            state.push(action.payload)
+        },
+        prepare(title, content) {
+            return {
+                payload : {
+                    id : nanoid(),
+                    title,
+                    content
+                }
+            }
+        }
     }
-});
+}});
 
 export const selectAllPosts = (state) => state.posts;
+
+export const {postAdded} = postSlice.actions
 
 export default postSlice.reducer
