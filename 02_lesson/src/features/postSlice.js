d609fee59c77545d1d369cd0ebe82b9bdd312332
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-23 17:32:38 +0100

    Update postSlice.js

--- 02_lesson/src/features/postSlice.js
+++ 02_lesson/src/features/postSlice.js
@@ -6,13 +6,27 @@ const initialState = [
         id : '1', 
         title : 'Learning Redux Toolkit', 
         content: "I've heard good thing ",
-        date : sub(new Date(), {minutes : 10}).toISOString()
+        date : sub(new Date(), {minutes : 10}).toISOString(),
+        reactions : {
+            thumbsUp : 0,
+            rocket : 0,
+            wow : 0,
+            heart : 0,
+            coffee : 0
+        }
     },
     {
         id : '2', 
         title : 'Slices...', 
         content : "The more I say slice, the more I want pizza.",
-        date : sub(new Date(), {minutes : 10}).toISOString()
+        date : sub(new Date(), {minutes : 10}).toISOString(),
+        reactions : {
+            thumbsUp : 0,
+            rocket : 0,
+            wow : 0,
+            heart : 0,
+            coffee : 0
+        }
     }
 ]
 
