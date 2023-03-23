5847ed9f2a246f14d61b6a116d20e00818abe8c2
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-23 19:33:26 +0100

    Update PostList.js

--- 02_lesson/src/features/PostList.js
+++ 02_lesson/src/features/PostList.js
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postSlice";
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
+import ReactionButton from "./ReactionButton";
 
 const PostList = () => {
     const posts = useSelector(selectAllPosts)
@@ -16,6 +17,7 @@ const PostList = () => {
                 <PostAuthor userId={post.userId}/>
                 <TimeAgo timestamp={post.date}/>   
             </p>
+            <ReactionButton post={post}/>
         </article>
     ))
 
